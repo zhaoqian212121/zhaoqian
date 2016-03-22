c5e7c1156e7f175799d29e5c4afa2ed8e449ab78
mac <mac@macdemacbook-air.local>
2016-03-22 12:07:02 -0700

    helloworld 1

---
+++ DDD/DDD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDD
+//
+//  Created by mac on 16/3/22.
+//  Copyright © 2016年 Mandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDD/DDD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDD
+//
+//  Created by mac on 16/3/22.
+//  Copyright © 2016年 Mandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
